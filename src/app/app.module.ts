33826463846e67b00dad41af9f802bd30eb91440
HenriqueGalvim <henrixe80@gmail.com>
2024-02-19 16:07:33 -0400

    Testando git flow em breve

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './pages/home/components/header/header.component';
+import { MembrosComponent } from './pages/home/components/membros/membros.component';
+import { NoticiasComponent } from './pages/home/components/noticias/noticias.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    MembrosComponent,
+    NoticiasComponent
   ],
   imports: [
     BrowserModule,
